f7d042a719c3237da175daaff9ad6230daacdedc
Mostafa1682002 <ma9856603@gmail.com>
2022-09-22 00:31:32 +0200

    Frist Commit

---
+++ JS/main.js
@@ -0,0 +1,48 @@
+let search=document.querySelector('.search');
+let searchForm=document.querySelector('header form');
+let user=document.querySelector('.user');
+let portfolioo=document.querySelector('.portfolioo');
+let menu=document.querySelector('.menu');
+let slidebar=document.querySelector('.slidebar');
+let btnClose=document.querySelector('.slidebar .close')
+//Seach Icon Click
+search.onclick=function(){
+    //Toggle from Search 
+    searchForm.classList.toggle('active');
+    //Remove active from portfolioo 
+    portfolioo.classList.remove('active');
+}
+//user Icon Click
+user.onclick=function(){
+    //Toggle portfolioo
+    portfolioo.classList.toggle('active');
+    //Remove active from Search 
+    searchForm.classList.remove('active');
+}
+
+//menu Icon Click
+menu.onclick=()=>{
+    //Toggle Slidebar
+    slidebar.classList.toggle('active');
+    document.body.classList.toggle('active');
+    //Remove active from Search 
+    searchForm.classList.remove('active');
+    //Remove active from portfolioo 
+    portfolioo.classList.remove('active');
+}
+//Close Slidebar
+btnClose.onclick=()=>{
+    slidebar.classList.remove('active');
+    document.body.classList.remove('active');
+}
+
+
+window.onscroll = () =>{
+    if(window.innerWidth < 1000){
+        slidebar.classList.remove('active');
+        document.body.classList.remove('active');
+    }
+}
+
+
+let year=document.querySelector('.year').innerHTML=new Date().getFullYear();
